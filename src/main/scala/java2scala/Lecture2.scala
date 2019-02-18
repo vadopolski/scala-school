2ba1e1e9a596d55cdd1921229ffa06963254ecc0
Oleg Nizhnik <odomontois@gmail.com>
2019-02-18 16:57:18 +0300

    lecture 2

--- src/main/scala/java2scala/Lecture2.scala
+++ src/main/scala/java2scala/Lecture2.scala
@@ -2,26 +2,61 @@ package java2scala
 
 object Lecture2 {
 
-  class Animal(val name: String){
-    override def toString = s"animal $name"
+  trait Food
+
+  abstract class Animal[Self, Eats](name: String, full: Double = 0) {
+    animal: Self =>
+    def eat(food: Eats): Self
+
+    def eatLegacy(food: Food): Animal[Self, Eats]
   }
 
-  object Animal{
-    def apply(idx: Int): Animal = new Animal(idx.toString)
+  case class Grass() extends Food
+
+  object Animal
+
+  case class Cow(name: String, full: Double = 0) extends Animal[Cow, Grass](name, full) with Food {
+
+    override def eatLegacy(food: Food): Cow =
+      if (food.isInstanceOf[Grass]) eat(food.asInstanceOf[Grass])
+      else {
+        println("I dont eat it")
+        this
+      }
+
+    override def eat(food: Grass): Cow = {
+      println("yummi")
+      Cow(name, full + 0.1)
+    }
+
   }
 
-  class Cow(val idx: Int) extends Animal(idx.toString) {
-    override def toString: String =
-      s"Cow $name $idx"
+  case class Lion(name: String, full: Double = 0) extends Animal[Lion, Cow](name, full) {
+    def eat(cow: Cow): Lion = {
+      println(s"that ${cow.name} was delicious, thank you")
+      Lion(name, full)
+    }
+
+    def eatLegacy(food: Food): Lion = food match {
+      case cow: Cow => eat(cow)
+
+      case _ =>
+        println("I dont eat it")
+        this
+    }
+
   }
 
   def main(args: Array[String]): Unit = {
-    val cow = new Cow(3)
-    val idx = cow.idx
-    val animal = Animal(5)
+    val cow   =  Cow("maggy")
+    val lion  = Lion("john")
+    val grass = Grass()
+    val cow2  = cow.eat(grass)
+    lion.eat(cow2)
 
+//    cow: cow2.type
 
     println(cow)
-    println(animal)
+    println(cow2)
   }
 }
