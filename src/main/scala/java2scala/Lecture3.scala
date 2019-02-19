baa08af9ac5dadb20b066a5bf5994a98a5cc43c8
Oleg Nizhnik <odomontois@gmail.com>
2019-02-19 20:38:28 +0300

    Colors

---
+++ src/main/scala/java2scala/Lecture3.scala
@@ -0,0 +1,45 @@
+package java2scala
+
+object Lecture3 extends App {
+  import Color._
+  sealed trait Color {
+    def name: String = this match {
+      case Red   => "red"
+      case Blue  => "blue"
+      case Green => "green"
+    }
+  }
+
+  object Color {
+    case object Red   extends Color
+    case object Blue  extends Color
+    case object Green extends Color
+  }
+
+//  def main(args: Array[String]): Unit = {
+//    println(Red.name)
+//    println(Green.name)
+//  }
+
+  sealed trait GenColor {
+    def show: String = this match {
+      case White              => "white"
+      case Black              => "black"
+      case Base(gen)          => s"base color ${gen.name}"
+      case Mix(first, second) => s"mix of ${first.name} and ${second.name}"
+    }
+  }
+
+  case object White                           extends GenColor
+  case object Black                           extends GenColor
+  case class Base(color: Color)               extends GenColor
+  case class Mix(first: Color, second: Color) extends GenColor
+
+
+//  case class Lol(override val color: Color) extends Base(color)
+
+  println(White.show)
+  println(Base(Green).show)
+  println(Mix(Red, Blue).show)
+
+}
---
+++ src/main/scala/java2scala/NotALecture.scala
@@ -0,0 +1,7 @@
+package java2scala
+
+object NotALecture {
+  import Lecture3.Color
+
+//  case object Black extends Color
+}
