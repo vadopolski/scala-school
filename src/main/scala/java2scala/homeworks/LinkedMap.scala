1c3baede623de41af418c3e7f240debfebb11b7d
Oleg Nizhnik <odomontois@gmail.com>
2019-02-19 18:51:20 +0300

    LinkedMap comments

--- src/main/scala/java2scala/homeworks/LinkedMap.scala
+++ src/main/scala/java2scala/homeworks/LinkedMap.scala
@@ -1,30 +1,51 @@
 package java2scala.homeworks
 
 sealed trait LinkedMap[K, V] extends Traversable[(K, V)] {
+
+  /** должен вернуть `false` если коллекция содержит хотя бы один элемент */
   override def isEmpty: Boolean = ???
 
+  /** должен вернуть `true` если коллекция содержит ключ `key` */
   def contains(key: K): Boolean = ???
 
+  /** возвращает Some со значением значения, если коллекция содержит ключ `key`
+    * и None если не содержит */
   def apply(key: K): Option[V] = ???
 
+  /** возвращает новый LinkedMap[K, V],
+    * в котором добавлено или изменено значение для ключа `key` на `value` */
   def update(key: K, value: V): LinkedMap[K, V] = ???
 
+  /** возвращает новый LinkedMap[K, V]
+    * состоящий из тех же позиций, но в обратном порядке */
   def reverse: LinkedMap[K, V] = ???
 
+  /** создаёт новый LinkedMap, состоящий из элементов `this` и `other`
+    * если какой-то ключ встречается в обеих коллекциях,
+    * может быть выбрано любое значение*/
   def ++(other: LinkedMap[K, V]): LinkedMap[K, V] = ???
 
+  /** создаёт новый LinkedMap , где ко всем значениям применена заданная функция */
   def mapValues[W](f: V => W): LinkedMap[K, W] = ???
 
+  /** создаёт новый LinkedMap , где ко всем значениям применена заданная функция,
+    * учитывающая ключ*/
   def mapWithKey[W](f: (K, V) => W): LinkedMap[K, W] = ???
 
+  /** конструирует новый LinkedMap, содеоржащий все записи текущего, кроме заданного ключа */
   def delete(key: K): LinkedMap[K, V] = ???
 
-  def foreach[U](kv: ((K, V)) => U): Unit = ???
-
-  override def toString: String = ???
+  /** применяет действие `action` с побочным эффектом ко всем элементам коллекции */
+  def foreach[U](action: ((K, V)) => U): Unit = ???
 }
 
 object LinkedMap {
+
+  /** конструирует новый `LinkedMap` на основании приведённых элементов
+    * каждый ключ должен присутствовать в результате только один раз
+    * если в исходных данныхх ключ встречается несколько раз, может быть
+    * выбрано любое из значений
+    */
   def apply[K, V](kvs: (K, V)*): LinkedMap[K, V] = ???
 
   final case class Cons[K, V](key: K, value: V, rest: LinkedMap[K, V]) extends LinkedMap[K, V]
