564eb60a3650353a5a859b3c088ec7db5f330607
Oleg Nizhnik <odomontois@gmail.com>
2019-02-19 21:04:51 +0300

    Colors

--- src/main/scala/java2scala/Lecture3.scala
+++ src/main/scala/java2scala/Lecture3.scala
@@ -20,13 +20,19 @@ object Lecture3 extends App {
 //    println(Red.name)
 //    println(Green.name)
 //  }
+//  val VeryRed = Mix(Red, Red)
+  val veryRed = Mix(Red, Red)
 
   sealed trait GenColor {
     def show: String = this match {
-      case White              => "white"
-      case Black              => "black"
-      case Base(gen)          => s"base color ${gen.name}"
-      case Mix(first, second) => s"mix of ${first.name} and ${second.name}"
+      case White                           => "white"
+      case Black                           => "black"
+      case Base(gen)                       => s"base color ${gen.name}"
+      case Mix(Green, Green)               => s"the greenest"
+      case `veryRed`                       => s"the redest"
+      case Mix(col1, col2) if col1 == col2 => s"very very $col1"
+      case Mix(Blue, col @ (Red | Green))  => s"little bit sad ${col.name}"
+      case Mix(first, second)              => s"mix of ${first.name} and ${second.name}"
     }
   }
 
@@ -35,11 +41,20 @@ object Lecture3 extends App {
   case class Base(color: Color)               extends GenColor
   case class Mix(first: Color, second: Color) extends GenColor
 
-
 //  case class Lol(override val color: Color) extends Base(color)
 
   println(White.show)
   println(Base(Green).show)
   println(Mix(Red, Blue).show)
+  println(Mix(Red, Red).show)
+  println(Mix(Blue, Red).show)
+  println(Mix(Green, Green).show)
 
+  println(null == Red)
+  println(Red == null)
+
+  println(100000 == (100000: Integer))
+  println(100000 eq (100000: Integer))
+  println(127 eq (127: Integer))
+  
 }
