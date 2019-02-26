27f843ad236e021e6040fdcbff1120881db9c51f
Oleg Nizhnik <odomontois@gmail.com>
2019-02-26 20:36:18 +0300

    namers

--- src/main/scala/java2scala/Lecture4.scala
+++ src/main/scala/java2scala/Lecture4.scala
@@ -1,11 +1,18 @@
 package java2scala
 
+import java2scala.Lecture3.{Base, Color, GenColor}
+import java2scala.Lecture3.Color.{Blue, Green, Red}
+
 import scala.Function.const
 
 trait ShowInt {
   def name(n: Int): String
 
   def name2(n: Int, m: Int): String = s"${name(n)} :: ${name(m)}"
+}
+
+case class Lol() {
+  def sdfsdf_+++++(lol: Lol): String = "lol"
 }
 
 object Lecture4 extends App {
@@ -36,19 +43,61 @@ object Lecture4 extends App {
   val constTrue: BoolToBool  = _ => true
   val constFalse: BoolToBool = const(false)
 
-  val plus3: Int => Int = x => x + 3
-  val plus4: Int => Int = _ + 4
-  val plus5: Int => Int = 5 +
+  val plus3: Int => Int  = x => x + 3
+  val plus4: Int => Int  = _ + 4
+  val plus5: Int => Int  = 5 +
+  val plus5a: Int => Int = x => 5.+(x)
+  val plus5b: Int => Int = 5.+
 
-  println(plus3(2))
-  println(plus4(2))
-  println(plus5(2))
+  val xx  = 5.+(1)
+  val xx2 = 5 + 1
+
+  val u = (i: Long) => 6 + i
+
+  def foo(int: Int, long: Long, str: String, double: Double): Boolean = int == long
+
+  val y  = namer.name2 _
+  val y1 = namer.name2(3, _)
+
+//  println(plus3(2))
+//  println(plus4(2))
+//  println(plus5(2))
+
+  def foo1 = foo _
+  def foo2 = foo(1, _, "sss", _)
 
   val namer: ShowInt = x => s"the int $x"
 
   def name3(x: Int, y: Int, namer: ShowInt) = println(namer.name2(x, y))
 
-  name3( -1, 4, x => s"the int $x")
+  name3(-1, 4, x => s"the int $x")
 
-  println(namer.name2(4, 5))
+  val colorToBool: Color => Boolean = {
+    case Red          => true
+    case Blue | Green => false
+  }
+
+  val genColorToString: GenColor => String = {
+    case Base(color) => s"base $color"
+    case _           => "not base"
+  }
+
+  val plus: Int => Int => Int  = x => y => x + y
+  val plus2: (Int, Int) => Int = (x, y) => x + y
+  def plus2a                   = plus2.curried
+
+  def plusM(x: Int)(y: Int) = x + y
+  def plusS(x: Int, y: Int) = x + y
+
+  def transformInt(x: Int, f: Int => Int) = f(x)
+  println(transformInt(6, plusS(4, _)))
+  println(transformInt(6, plusM(4)))
+
+    
+
+
+//  println(plus(2)(3))
+//  println(plus2a(2)(3))
+
+//  println(namer.name2(4, 5))
 }
