fb58b5b860a60ccd820c349e3cd1f903de74670f
Oleg Nizhnik <odomontois@gmail.com>
2019-03-12 19:55:36 +0300

    lect6

---
+++ src/main/scala/java2scala/Lecture6.scala
@@ -0,0 +1,16 @@
+package java2scala
+import scala.collection.immutable.{Seq, IndexedSeq}
+//
+object Lecture6 extends App {
+//  println(List("foo", "bar", "baz").zipWithIndex)
+  
+
+//  val res = for {
+//    x      <- Vector(1, 2, 3)
+//    list   = List.range(x, 0, -1)
+//    (y, i) <- list.zipWithIndex
+//  } yield s" {x = $x,  y = $y, i = $i}"
+//
+  res.foreach(println)
+  println(res)
+}
