25dd8c030274bf9921db2e5849d30326e2931e7f
Oleg Nizhnik <odomontois@gmail.com>
2019-03-19 20:42:48 +0300

    ------------

---
+++ src/main/scala/java2scala/Lecture7.scala
@@ -0,0 +1,36 @@
+package java2scala
+
+import cats.effect.{ExitCode, IO, IOApp, Timer}
+
+import scala.concurrent.duration._
+import scala.io.StdIn
+import scala.language.postfixOps
+import cats.implicits._
+
+object Lecture7 extends IOApp {
+  val helloWorld = IO(println("Hello world"))
+
+  def printlnIO(str: String): IO[Unit] = IO(println(str))
+  val readLn: IO[String]               = IO(StdIn.readLine()) //TODO : make async !!!
+
+  def elapsed(seconds: Long = 0): IO[Nothing] =
+    for {
+      _   <- printlnIO(s"$seconds seconds elapsed")
+      _   <- Timer[IO].sleep(1 second)
+      res <- elapsed(seconds + 1)
+    } yield res
+
+  val waitForEnter = printlnIO("press Enter to exit") <* readLn
+
+  val app = for {
+    _    <- printlnIO("write your name: ")
+    name <- readLn
+    _    <- printlnIO(s"Hello, $name")
+  } yield ExitCode.Success
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- (Timer[IO].sleep(1 millisecond) *> elapsed()).start
+      _ <- waitForEnter
+    } yield ExitCode.Success
+}
