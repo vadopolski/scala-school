b929ae7a081a6362dc4a56fdf3307f1fb56450f9
Oleg Nizhnik <odomontois@gmail.com>
2019-03-26 20:12:36 +0300

    basic server

--- src/main/scala/java2scala/Lecture8.scala
+++ src/main/scala/java2scala/Lecture8.scala
@@ -5,12 +5,39 @@ import akka.http.scaladsl.server._
 import Directives._
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
 import akka.stream.{ActorMaterializer, Materializer}
-import cats.effect
-
 import cats.implicits._
+
+import scala.collection.immutable
 object Lecture8 extends IOApp {
-  val route: Route = complete("Hello World")
+  val route: Route =
+    handleRejections(RejH) {
+      path("") {
+        complete("Hello World")
+      } ~
+        pathPrefix("users") {
+          path("one") {
+            complete("Hello, One")
+          } ~
+            path("two") {
+              complete("Hello, Two")  
+            } ~
+            path("three") {
+              reject(MyRejection)
+            }
+        }
+    }
+
+  case object MyRejection extends Rejection
+
+  object RejH extends RejectionHandler {
+    def apply(rejs: immutable.Seq[Rejection]): Option[Route] =
+      if (rejs.contains(MyRejection))
+        Some(complete(StatusCodes.NotImplemented, "lol no"))
+      else None
+
+  }
 
   implicit val system: ActorSystem        = ActorSystem("our-server")
   implicit val materializer: Materializer = ActorMaterializer()
