9a13909fd94a565c650f285caf7ad0fca029648d
Oleg Nizhnik <odomontois@gmail.com>
2019-03-26 20:54:33 +0300

    state

--- src/main/scala/java2scala/Lecture8.scala
+++ src/main/scala/java2scala/Lecture8.scala
@@ -5,24 +5,40 @@ import akka.http.scaladsl.server._
 import Directives._
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.ToResponseMarshaller
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.{ActorMaterializer, Materializer}
+import cats.effect.concurrent.Ref
 import cats.implicits._
 
 import scala.collection.immutable
 object Lecture8 extends IOApp {
-  val route: Route =
+
+//  implicit val ioListStringMarshaller: ToResponseMarshaller[IO[List[String]]] = ???
+
+  def route(tags: AppTags): Route =
     handleRejections(RejH) {
       path("") {
         complete("Hello World")
       } ~
+        pathPrefix("tags") {
+          get { complete(tags.allTags.map(_.toString()).unsafeToFuture()) } ~
+            (put & parameter("tag")) { tag =>
+              complete(tags.addTag(tag).as("ok added").unsafeToFuture())
+            }
+        } ~
         pathPrefix("users") {
-          parameter("id".as[Int]) { id =>
-            complete(s"hello, $id")
+          parameters(("id".as[Int].?, "version".?)) { (id, version) =>
+            complete(s"id = $id version = $version")
           }
         } ~
         pathPrefix("products" / IntNumber / LongNumber / Segment) { (num, num2, str) =>
           complete(s"product[$num, $num2] : $str") // http://localhost:8080/products/3/5/etert
+        } ~
+        pathPrefix("foo") {
+          parameterMultiMap { pm =>
+            complete(pm.get("bar").toString)
+          }
         }
     }
 
@@ -33,12 +49,26 @@ object Lecture8 extends IOApp {
       if (rejs.contains(MyRejection))
         Some(complete(StatusCodes.NotImplemented, "lol no"))
       else None
-
   }
 
   implicit val system: ActorSystem        = ActorSystem("our-server")
   implicit val materializer: Materializer = ActorMaterializer()
 
   def run(args: List[String]): IO[ExitCode] =
-    IO.fromFuture(IO(Http().bindAndHandle(route, "0.0.0.0", 8080))) as ExitCode.Success
+    for {
+      tags     <- AppTags(Nil)
+      appRoute = route(tags)
+      _        <- IO.fromFuture(IO(Http().bindAndHandle(appRoute, "0.0.0.0", 8080)))
+    } yield ExitCode.Success
+
+}
+
+final class AppTags(ref: Ref[IO, List[String]]) {
+  def allTags: IO[List[String]]     = ref.get
+  def addTag(tag: String): IO[Unit] = ref.update(tag :: _)
+}
+
+object AppTags {
+  def apply(initial: List[String]): IO[AppTags] =
+    for (ref <- Ref[IO].of(initial)) yield new AppTags(ref)
 }
