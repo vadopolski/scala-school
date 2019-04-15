0de63d2b5ded20e0d381db97d90ae4c823776508
Oleg Nizhnik <odomontois@gmail.com>
2019-04-15 19:38:44 +0300

    filter AND

--- src/main/scala/java2scala/tagless/Filter.scala
+++ src/main/scala/java2scala/tagless/Filter.scala
@@ -15,20 +15,25 @@ object Filter extends App {
   def show(filter: Filter): String = filter match {
     case ColorIs(color) => s"color = $color"
     case PriceLT(upper) => s"price <= $upper"
+    case And(x, y)      => s"${show(x)} AND ${show(y)} "
   }
 
-  def filter(filter: Filter)(product: Product): Boolean = filter match {
+  def filter(flt: Filter)(product: Product): Boolean = flt match {
     case ColorIs(color) => product.color == color
     case PriceLT(upper) => product.price <= upper
+    case And(x, y)      => filter(x)(product) && filter(y)(product)
   }
 
   val isRed: Filter = ColorIs("red")
   val priceLT100    = PriceLT(100)
+  val combined      = And(isRed, priceLT100)
 
   println(show(isRed))
   println(show(priceLT100))
+  println(show(combined))
 
   val products = List(Product("yellow", 50), Product("red", 50), Product("red", 200))
   println(products.filter(filter(isRed)))
   println(products.filter(filter(priceLT100)))
+  println(products.filter(filter(combined)))
 }
--- src/main/scala/java2scala/tagless/FilterTF.scala
+++ src/main/scala/java2scala/tagless/FilterTF.scala
@@ -3,6 +3,7 @@ package java2scala.tagless
 trait FilterTF[T] {
   def colorIs(color: String): T
   def priceLT(upper: BigDecimal): T
+  def and(x: T, y: T): T
 }
 
 object FilterTF extends App {
@@ -17,22 +18,30 @@ object FilterTF extends App {
     new FilterTF[String] {
       def colorIs(color: String): String     = s"color = $color"
       def priceLT(upper: BigDecimal): String = s"price <= $upper"
+      def and(x: String, y: String): String  = s"$x AND $y"
     }
+
+//  type ProductPred = Product => Boolean
 
   implicit def filterInt: FilterTF[Product => Boolean] =
     new FilterTF[Product => Boolean] {
       def colorIs(color: String): Product => Boolean     = _.color == color
       def priceLT(upper: BigDecimal): Product => Boolean = _.price <= upper
+      def and(x: Product => Boolean, y: Product => Boolean): Product => Boolean =
+        p => x(p) && y(p)
     }
 
   def isRed[T: FilterTF]: T   = colorIs[T]("red")
   def priceLT100[T: FilterTF] = FilterTF[T].priceLT(100)
+  def combined[T: FilterTF]   = FilterTF[T].and(isRed[T], priceLT100[T])
 
   println(isRed[String])
   println(priceLT100[String])
+  println(combined[String])
 
   val products = List(Product("yellow", 50), Product("red", 50), Product("red", 200))
   println(products.filter(isRed[Product => Boolean]))
   println(products.filter(priceLT100[Product => Boolean]))
+  println(products.filter(combined[Product => Boolean]))
 
 }
